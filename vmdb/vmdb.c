af9f86e2bb6746067495d949a925e3af855bb694
wutakenmyname <46101097+wutakenmyname@users.noreply.github.com>
2024-03-31 10:31:21 +0000

    add sem support

--- vmdb/vmdb.c
+++ vmdb/vmdb.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <string.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
 
 #define VMDB_BASE_FILE_PATH "/root/vmdb_file"
 #define VMDB_RANDON_ID 0x1234
@@ -16,12 +17,19 @@ typedef struct
     uint16 size;
 }data_info_t;
 
+union semun {
+    int val;                // 用于 SETVAL 操作
+    struct semid_ds *buf;   // 用于 IPC_STAT 和 IPC_SET 操作
+    unsigned short *array;  // 用于 GETALL 和 SETALL 操作
+};
+
 key_t vmdb_key;
 key_t sem_key;
 base_db_t base_db;
 customization_t *global_data = NULL;
 unsigned char *generated_data = NULL;
 int generated_data_length = -1;
+int vmdb_sem_fd = -1;
 
 #define GET_OFFSET(mstruct, member) ((size_t) &(((mstruct *)0)->member))
 #define GET_SIZE(mstruct, member) sizeof(((mstruct *)0)->member)
@@ -74,19 +82,54 @@ void restore_data_from_file(char *file_data, int file_data_len)
     // to do, parse data
 }
 
-void vm_lock_init()
+STATUS_T vmdb_lock()
 {
-    return;
+    struct sembuf sem_buf;
+    sem_buf.sem_num = 0;
+    sem_buf.sem_op = -1;
+    sem_buf.sem_flg = SEM_UNDO;//系统退出前未释放信号量，系统自动释放
+    if (semop(vmdb_sem_fd, &sem_buf, 1) == -1) {
+        mprintf("lock vmdb sem failed\n");
+        return STATUS_NOK;
+    }
+    return STATUS_OK;
 }
 
-void vmdb_lock()
+STATUS_T vmdb_unlock()
 {
-    return;
+    struct sembuf sem_buf;
+    sem_buf.sem_num = 0;
+    sem_buf.sem_op = 1;
+    sem_buf.sem_flg = SEM_UNDO;//系统退出前未释放信号量，系统自动释放
+    if (semop(vmdb_sem_fd, &sem_buf, 1) == -1) {
+        mprintf("unlock vmdb sem failed\n");
+        return STATUS_NOK;
+    }
+    return STATUS_OK;
 }
 
-void vmdb_unlock()
+static STATUS_T vm_lock_init()
 {
-    return;
+    vmdb_sem_fd = semget(sem_key, 1, IPC_CREAT | 0666);
+    if (vmdb_sem_fd < 0)
+    {
+        mprintf("semget failed, return value :%d\n", vmdb_sem_fd);
+        return STATUS_NOK;
+    }
+
+    union semun val;
+    val.val = 1;
+    if (semctl(vmdb_sem_fd, 0, SETVAL, val) < 0)
+    {
+        mprintf("semctl failed\n");
+        return STATUS_NOK;
+    }
+
+    mprintf("going to get vm lock ready\n");
+    vmdb_lock();
+    vmdb_unlock();
+    mprintf("vm lock is ready");
+    return STATUS_OK;
 }
 
 STATUS_T vmdb_init_once()
@@ -94,7 +137,14 @@ STATUS_T vmdb_init_once()
     STATUS_T ret = STATUS_OK;
     vmdb_key = generate_vmdb_shm_key();
     sem_key = generate_vmdb_sem_key();
-    vm_lock_init();
+
+    ret = vm_lock_init();
+    if (ret != STATUS_OK)
+    {
+        mprintf("vm_lock_init failed\n");
+        return STATUS_NOK;
+    }
+
     printf("vmdb_key :%d\n", vmdb_key);
     mprintf("sizeof(customization_t): %d\n", sizeof(customization_t));
 
